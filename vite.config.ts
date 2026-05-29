8dfd50d751d89b136b0f40d0824633713056f43f
OkkieX0 <gabriel.lavaggi@hrastudent.ca>
2026-05-29 12:40:22 -0700

    pleasee

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change this to your preferred port
+    port: 4000, // Change this to your preferred port
   },
 
 })
