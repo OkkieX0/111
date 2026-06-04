2ba84812223d38df3ad70259fc27772298d1b14a
OkkieX0 <gabriel.lavaggi@hrastudent.ca>
2026-06-04 12:52:12 -0700

    pot chane 1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Replace 3000 with your preferred port number
+    strictPort: true, // Prevents Vite from automatically picking another port if 3000 is busy
+  },
 
 })
